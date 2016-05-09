da75e8a15fbebdab0028a994328dfa83dd5147d8
jamierose <jrose111284@gmail.com>
2016-05-09 16:14:38 +0100

    app is working has a few yellow errors i need to ammend. but the function of the app runs

--- is my number prime/ViewController.swift
+++ is my number prime/ViewController.swift
@@ -25,6 +25,35 @@ class ViewController: UIViewController {
     }
 
     @IBAction func click(sender: AnyObject) {
+//        var number = 2
+        var numberInt = Int(number.text!)
+        
+        if numberInt != nil {
+            var isPrime = true
+            
+            if numberInt == 1 {
+                isPrime = false
+            }
+            
+            if numberInt != 2 && numberInt != 1{
+                
+                for var i = 2; i < numberInt; i++ {
+                    if numberInt! % i == 0 {
+                        isPrime = false
+                    }
+                }
+            }
+            
+            if isPrime == true {
+                answe.text = "\(numberInt!) is prime"
+            } else {
+                answe.text = "\(numberInt!) is not prime"
+            }
+
+        } else {
+            answe.text = "please enter a number"
+        }
+        
         
     }
 
