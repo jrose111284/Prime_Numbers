26d9be9ff88206a5c60fa2e26932e5da48757391
jamierose <jrose111284@gmail.com>
2016-05-09 15:30:44 +0100

    Initial Commit

---
+++ is my number prime/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  is my number prime
+//
+//  Created by Jamie Rose on 09/05/2016.
+//  Copyright © 2016 Jamie Rose. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
